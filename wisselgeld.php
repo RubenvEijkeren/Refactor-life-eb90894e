d9832477d97f3ec539393bd12d2626b3ba35f44c
RubenvEijkeren <54809240+RubenvEijkeren@users.noreply.github.com>
2019-11-20 15:21:20 +0100

    Uitwerking van de exercise

---
+++ wisselgeld.php
@@ -0,0 +1,39 @@
+<?php
+	define("GELDEENHEDEN", [50, 20, 10, 5, 2, 1, 0.5, 0.2, 0.1, 0.05]);
+	$input = round($argv[1]/0.05)*0.05;
+	isValid($input);
+	calcChange($input);
+
+	function isValid($e){
+		if ($e == 0){
+		echo "Geen wisselgeld\n";
+		exit;
+		}
+	}
+	function calcChange($bedrag){
+		foreach (GELDEENHEDEN as $key => $value) {
+//			echo "current value: " . $value . "\n";
+			if ($bedrag > $value && $value	 > 1){
+				$aantal = intval($bedrag / $value);
+				$bedrag = round(($bedrag*100) % ($value*100))/100;
+				echo $aantal . " x " . $value . " euro\n";
+			}
+			if ($bedrag > $value && $value < 1){
+				$aantal = intval($bedrag / $value);
+	//			echo "current aantal: " . $aantal . "\n";
+				$bedrag = round(($bedrag*100) % ($value*100))/100;
+	//			echo "current bedrag: " . $bedrag . "\n";
+				echo $aantal . " x " . $value*100 . " cent\n";
+			}
+			if ($bedrag == 1){
+				echo "1 x 1 euro\n";
+				exit;
+			}
+			if ($bedrag == 0.05){
+				echo "1 x 5 cent";
+				exit;
+			}
+		}
+	}
+
+?>
